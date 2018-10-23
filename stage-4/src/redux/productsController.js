93e3bbdd7c037dcb5c45d69410a30772b80008fa
Patric Khiev <pat@Patrics-MacBook-Pro.local>
2018-10-23 16:13:15 -0500

    project complete

--- stage-4/src/redux/productsController.js
+++ stage-4/src/redux/productsController.js
@@ -1,6 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
-const getAllProducts = function() {
-    return axios.get("https://practiceapi.devmountain.com/products/")
+export const getAllProducts = function() {
+  return axios
+    .get("https://practiceapi.devmountain.com/products/")
     .then(response => response.data);
-}
+};
+
